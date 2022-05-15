86148e7952eac1f64d3cd9341592b5138b79e73b
AnkushRawat <ankushrwt344@gmail.com>
2022-05-15 09:41:33 +0530

    Uploading ValidateEmail in Uc3 branch

--- RFP_DAY19_REGEX/Program.cs
+++ RFP_DAY19_REGEX/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY19_REGEX;
 Console.WriteLine("Welcome to REGEX PROGRAM!\n");
-Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
-switch(num)
+switch (num)
 {
     case 1:
         ValidFirstName nameObj = new ValidFirstName();
@@ -28,5 +28,17 @@ switch(num)
             Console.WriteLine("Invalid");
         }
         break;
-}
 
+    case 3:
+        ValidateEmail EmailObj = new ValidateEmail();
+        if (EmailObj.Validate_Email("abc.xyz@bl.co.in"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
+}
